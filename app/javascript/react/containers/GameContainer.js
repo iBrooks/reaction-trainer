9808f9216141ca7b245361b7338c96d4013a11e0
Ian Brooks <ianphilipbrooks@gmail.com>
2018-01-22 09:24:07 -0500

    Created timer that doesnt work

--- app/javascript/react/containers/GameContainer.js
+++ app/javascript/react/containers/GameContainer.js
@@ -6,33 +6,92 @@ class Game extends Component{
   constructor(props){
     super(props)
     this.state = {
-      location: 'left'
+      location: 'left',
+      times: [],
+      gameLength: 10,
+      count: 0,
+      gameState: 'ready'
     }
     this.onClick = this.onClick.bind(this)
+    this.timer = this.timer.bind(this)
   }
+
+  timer(action=nil){
+    let mSec, t
+    let times = []
+
+    if(action == 'next') {
+      clearTimeout(t)
+      console.log(mSec)
+      times.push(mSec)
+      mSec = 0
+      t = setTimeout(()=>{ mSec++ }, 1)
+    } else if(action == 'stop') {
+      clearTimeout(t)
+      times.push(mSec)
+      mSec = 0
+      this.setState({
+          times: times,
+          gameState: 'ended'
+        })
+      times = []
+    } else {
+      this.setState({ gameState: 'running', times: [] })
+      mSec = 0
+      t = setTimeout((mSec)=>{ mSec++ }, 100)
+    }
+  }
+
   onClick(event){
-    console.log(this.state.location)
     event.preventDefault()
+
     let newTargetLocation
-    if (this.state.location = 'left') {
+    if (this.state.location == 'left') {
       newTargetLocation = 'right'
     } else {
       newTargetLocation = 'left'
     }
-    this.setState({
-      location: newTargetLocation
-    })
+
+    if (this.state.count == this.state.gameLength) {
+      this.timer('stop')
+    } else {
+      this.timer('next')
+      this.setState({
+        location: newTargetLocation,
+        count: this.state.count + 1,
+        gameState: 'running'
+      })
+    }
+
   }
+
   render(){
-    let onButtonClick = this.onClick
-    return(
-    <div className='container'>
-      <Target
-        location={this.state.location}
-        onClick={onButtonClick}
-      />
-    </div>
-  )
+    if (this.state.gameState == 'ready'){
+      let onClick = this.timer
+      return(
+        <div>
+          <h1 onClick={onClick}>Start!</h1>
+        </div>
+      )
+    } else if (this.state.gameState == 'running') {
+      let onButtonClick = this.onClick
+      return(
+        <div className='container'>
+          <Target
+            location={this.state.location}
+            onClick={onButtonClick}
+          />
+        </div>
+      )
+    } else {
+      return(
+        <div>
+          <h1>Complete!</h1>
+          <h4>Raw times:</h4>
+          <p>{this.state.times.join}</p>
+        </div>
+      )
+    }
   }
 }
 
