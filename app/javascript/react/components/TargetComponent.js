a59b3521c03049e30a2f67b12edeec7545025a1a
Ian Brooks <ianphilipbrooks@gmail.com>
2018-02-06 14:51:48 -0500

    fixed the positional grid added location randomizer

--- app/javascript/react/components/TargetComponent.js
+++ app/javascript/react/components/TargetComponent.js
@@ -2,9 +2,8 @@ import React from 'react'
 
 const Target = (props) => {
   return(
-    <div className={props.location}>
-      <i className="fa fa-square fa-3x" onClick={props.onHit}></i>
-    </div>
+      <div className={props.location} id='target' onClick={props.onHit}>
+      </div>
   )
 }
 
