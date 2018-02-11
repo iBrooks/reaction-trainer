ff31a2336d700c24977e6187fa446f3478d8b1fd
Ian Brooks <ianphilipbrooks@gmail.com>
2018-02-11 14:53:41 -0500

    migration written, issue with local postgress sever

---
+++ db/migrate/20180211194533_move_targets_to_game.rb
@@ -0,0 +1,17 @@
+class MoveTargetsToGame < ActiveRecord::Migration[5.1]
+  def up
+    drop_table target_hits
+
+    add_column :games, :target_times, integer[]
+    end
+  end
+  def down
+    remove_column :games, :target_times
+
+    create_table :target_hits do |t|
+      t.belongs_to :game, null: false
+      t.references :user, through: :game, null: true
+      t.integer :ms, null: false
+    end
+  end
+end
