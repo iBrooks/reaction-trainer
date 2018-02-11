c6ea6ed85d272b66dc1a4a57927d9033244f4ce4
Ian Brooks <ianphilipbrooks@gmail.com>
2018-02-11 15:19:29 -0500

    added user_name to users and added validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :games
+  validates :user_name, presence: true, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
