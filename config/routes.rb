131b3122d54beac463e13dbfb265d5503046d744
Ian Brooks <ianphilipbrooks@gmail.com>
2018-02-12 15:38:51 -0500

    adjusted routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,5 @@ Rails.application.routes.draw do
       resource :users, only: [:show]
     end
   end
-  devise_scope :user do
-    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
